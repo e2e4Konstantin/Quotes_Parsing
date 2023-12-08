6dc1cefda4bc88de29cd7f0924be44ed37c12f6c
Konstantin Kazak <e2e4suchok@gmail.com>
2023-12-08 10:15:01 +0300

    db attributes queries 2

--- db_queries/attributes_queries.sql
+++ db_queries/attributes_queries.sql
@@ -1,18 +1,35 @@
--- Таблица 4.8-288. Трубы гофрированные поливинилхлоридные открыто по стенам и потолкам с установкой соединительных коробок
-select c.ID_tblCatalog, c.* from tblCatalogs c
-where c.FK_tblCatalogs_tblDirectoryItems = (
-        select di.ID_tblDirectoryItem from tblDirectoryItems di where di.name = 'Таблица'
-    )   and c.code REGEXP '^4\.8-\d+-\d+-0-288';
--- 4830
+SELECT c.ID_tblCatalog,  c.*
+FROM tblCatalogs c
+WHERE
+  c.FK_tblCatalogs_tblDirectoryItems = (
+      SELECT di.ID_tblDirectoryItem FROM tblDirectoryItems di WHERE di.name = 'Таблица'
+    )
+  AND c.code REGEXP '^4\.8-\d+-\d+-0-288';
 
--- получить все Уникальные значения атрибутов с названием "Способ" a.name = 'Способ' and
-select a.* from tblAttributes a where a.FK_tblAttributes_tblQuotes in
-    (select q.ID_tblQuote from tblQuotes q where q.FK_tblQuotes_tblCatalogs = 4830);
+SELECT a.*
+FROM  tblAttributes a
+WHERE
+  a.FK_tblAttributes_tblQuotes IN (SELECT q.ID_tblQuote FROM tblQuotes q WHERE q.FK_tblQuotes_tblCatalogs = 4830);
 
-select distinct a.name from tblAttributes a where a.FK_tblAttributes_tblQuotes in (select q.ID_tblQuote from tblQuotes q where q.FK_tblQuotes_tblCatalogs = 4830);
--- Материал, Способ, Тип, Элемент
+SELECT DISTINCT a.name
+FROM tblAttributes a
+WHERE
+  a.FK_tblAttributes_tblQuotes IN (
+      SELECT q.ID_tblQuote FROM tblQuotes q WHERE q.FK_tblQuotes_tblCatalogs = 4830);
 
 
-select distinct a.value from tblAttributes a where a.name = 'Способ' and a.FK_tblAttributes_tblQuotes in (select q.ID_tblQuote from tblQuotes q where q.FK_tblQuotes_tblCatalogs = 4830);
+SELECT DISTINCT a.value
+FROM tblAttributes a
+WHERE
+  a.name = 'Способ'
+  AND a.FK_tblAttributes_tblQuotes IN (
+      SELECT q.ID_tblQuote FROM tblQuotes q WHERE q.FK_tblQuotes_tblCatalogs = 4830);
 
-select count(distinct a.value) from tblAttributes a where a.name = 'Способ' and a.FK_tblAttributes_tblQuotes in (select q.ID_tblQuote from tblQuotes q where q.FK_tblQuotes_tblCatalogs = 4830);
+
+SELECT COUNT(DISTINCT a.value)
+FROM tblAttributes a
+WHERE
+  a.name = 'Способ'
+  AND a.FK_tblAttributes_tblQuotes IN (
+      SELECT q.ID_tblQuote FROM tblQuotes q WHERE q.FK_tblQuotes_tblCatalogs = 4830
+    );
